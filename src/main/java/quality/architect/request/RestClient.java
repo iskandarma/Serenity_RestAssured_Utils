e77918dc3c7e07a5ea9b999368ae990120d77f43
iskandarma27 <iskandar@alterra.id>
2020-10-02 14:06:45 +0700

    - add get request

--- src/main/java/quality/architect/request/RestClient.java
+++ src/main/java/quality/architect/request/RestClient.java
@@ -3,7 +3,6 @@ package quality.architect.request;
 import io.restassured.response.Response;
 import net.serenitybdd.rest.SerenityRest;
 import quality.architect.spec.SpesificationFactory;
-import java.util.*;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
@@ -19,18 +18,6 @@ public class RestClient {
                 .statusCode(resCode);
     }
 
-    public void doGetRequest(String url, String token, int resCode, boolean log) {
-        if (log == true) {
-            SerenityRest
-                    .given()
-                    .spec(SpesificationFactory.requestSpecJson(token))
-                    .when()
-                    .get(url)
-                    .then()
-                    .statusCode(resCode);
-        }
-    }
-
     public void doGetRequest(String url, String token, int resCode, String pathJsonSchema) {
         SerenityRest
                 .given()
@@ -42,13 +29,29 @@ public class RestClient {
                 .statusCode(resCode);
     }
 
-    public Response doPostRequest(String path, Object body){
-        return SerenityRest
+    public void doGetRequest(String url, String token, int resCode, boolean log) {
+        if (log == true) {
+            SerenityRest
+                    .given()
+                    .spec(SpesificationFactory.requestSpecJson(token))
+                    .when()
+                    .get(url)
+                    .then()
+                    .log()
+                    .all()
+                    .statusCode(resCode);
+        }
+    }
+
+    public void doPostRequest(String url, String token, Object body, int resCode){
+        SerenityRest
                 .given()
-                .spec(SpesificationFactory.requestSpecJson())
+                .spec(SpesificationFactory.requestSpecJson(token))
                 .body(body)
                 .when()
-                .post(path);
+                .post(url)
+                .then()
+                .statusCode(resCode);
     }
 
     public Response doPostRequest(String path, Object body, String token){
