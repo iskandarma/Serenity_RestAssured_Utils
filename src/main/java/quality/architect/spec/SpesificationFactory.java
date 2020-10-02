2343c663bf23f4e009475e90802432d8248d505a
iskandarma27 <iskandar@alterra.id>
2020-10-02 15:47:43 +0700

    - add xml and formdata request SpesificationFactory

--- src/main/java/quality/architect/spec/SpesificationFactory.java
+++ src/main/java/quality/architect/spec/SpesificationFactory.java
@@ -1,6 +1,8 @@
 package quality.architect.spec;
 
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
@@ -23,4 +25,37 @@ public class SpesificationFactory {
         requestSpecification = requestSpecBuilder.build();
         return requestSpecification;
     }
+
+    public RequestSpecification requestSpecXml(){
+        requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setContentType(ContentType.XML);
+        requestSpecification = requestSpecBuilder.build();
+        return requestSpecification;
+    }
+
+    public RequestSpecification requestSpecXml(String token){
+        requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setContentType(ContentType.XML);
+        requestSpecBuilder.addHeader("Authorization", "Bearer "+token);
+        requestSpecification = requestSpecBuilder.build();
+        return requestSpecification;
+    }
+
+    public RequestSpecification requestSpecFormData(){
+        requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setUrlEncodingEnabled(true);
+        requestSpecBuilder.setConfig(RestAssuredConfig.config().encoderConfig(EncoderConfig.encoderConfig().encodeContentTypeAs("multipart/form-data", ContentType.TEXT)));
+        requestSpecification = requestSpecBuilder.build();
+        return requestSpecification;
+    }
+
+    public RequestSpecification requestSpecFormData(String token){
+        requestSpecBuilder = new RequestSpecBuilder();
+        requestSpecBuilder.setUrlEncodingEnabled(true);
+        requestSpecBuilder.setConfig(RestAssuredConfig.config().encoderConfig(EncoderConfig.encoderConfig().encodeContentTypeAs("multipart/form-data", ContentType.TEXT)));
+        requestSpecBuilder.addHeader("Authorization", "Bearer "+token);
+        requestSpecification = requestSpecBuilder.build();
+        return requestSpecification;
+    }
+
 }
