348128509ea2fa8cc2f82a9a9896dce82cc9c89c
iskandarma27 <iskandar@alterra.id>
2020-10-24 07:14:28 +0700

    - Add method put with json schema path in Interface class

--- src/main/java/quality/architect/request/Request.java
+++ src/main/java/quality/architect/request/Request.java
@@ -124,4 +124,124 @@ public interface Request {
          */
     void postMultipart(String url, String username, String password, Object body, int resCode, String jsonSchemaPath);
 
+        /**
+         * Put request using JWT Token for authentication and JSON for request body
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The JSON body request to send.
+         * @param resCode The response code.
+         */
+        void put(String url, String token, Object body, int resCode);
+
+        /**
+         * Put request using JWT Token for authentication and JSON for request body
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The JSON body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+        void put(String url, String token, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Put request using basic auth for authentication and JSON for request body
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The JSON body request to send.
+         * @param resCode The response code.
+         */
+        void put(String url, String username, String password, Object body, int resCode);
+
+        /**
+         * Put request using basic auth for authentication and JSON for request body
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The JSON body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+        void put(String url, String username, String password, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Put request using JWT Token for authentication and XML for request body
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The XML body request to send.
+         * @param resCode The response code.
+         */
+        void putXml(String url, String token, Object body, int resCode);
+
+        /**
+         * Put request using JWT Token for authentication and XML for request body
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The XML body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+        void putXml(String url, String token, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Put request using basic auth for authentication and XML for request body
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The XML body request to send.
+         * @param resCode The response code.
+         */
+        void putXml(String url, String username, String password, Object body, int resCode);
+
+        /**
+         * Put request using basic auth for authentication and XML for request body
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The XML body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+        void putXml(String url, String username, String password, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Put request using JWT Token for authentication and Multipart/form-data for request body
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The Multipart/form-data body request to send.
+         * @param resCode The response code.
+         */
+        void putMultipart(String url, String token, Object body, int resCode);
+
+        /**
+         * Put request using JWT Token for authentication and Multipart/form-data for request body
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The Multipart/form-data body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+        void putMultipart(String url, String token, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Put request using basic auth for authentication and Multipart/form-data for request body
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The Multipart/form-data body request to send.
+         * @param resCode The response code.
+         */
+        void putMultipart(String url, String username, String password, Object body, int resCode);
+
+        /**
+         * Put request using basic auth for authentication and Multipart/form-data for request body
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The Multipart/form-data body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+        void putMultipart(String url, String username, String password, Object body, int resCode, String jsonSchemaPath);
+
 }
