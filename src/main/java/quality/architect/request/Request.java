b66038dc0631e5f77b83baf1cb6a196347150ba2
iskandarma27 <iskandar@alterra.id>
2020-10-24 22:39:23 +0700

    - Add method patch with json schema path in Interface class

--- src/main/java/quality/architect/request/Request.java
+++ src/main/java/quality/architect/request/Request.java
@@ -317,4 +317,160 @@ public interface Request {
          */
     void delete(String url, String username, String password, int resCode, String jsonSchemaPath);
 
+        /**
+         * Patch request using JWT Token for authentication.
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param resCode The response code.
+         */
+    void patch(String url, String token, int resCode);
+
+        /**
+         * Patch request using JWT Token for authentication and JSON Schema Path.
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+    void patch(String url, String token, int resCode, String jsonSchemaPath);
+
+        /**
+         * Patch request using basic auth for authentication.
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param resCode The response code.
+         */
+    void patch(String url, String username, String password, int resCode);
+
+        /**
+         * Patch request using basic auth for authentication and JSON Schema Path.
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+    void patch(String url, String username, String password, int resCode, String jsonSchemaPath);
+
+        /**
+         * Patch request using JWT Token for authentication and JSON for request body.
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The JSON body request to send.
+         * @param resCode The response code.
+         */
+    void patch(String url, String token, Object body, int resCode);
+
+        /**
+         * Patch request using JWT Token for authentication and JSON for request body.
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The JSON body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+    void patch(String url, String token, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Patch request using basic auth for authentication and JSON for request body usage.
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The JSON body request to send.
+         * @param resCode The response code.
+         */
+    void patch(String url, String username, String password, Object body, int resCode);
+
+        /**
+         * Patch request using basic auth for authentication and JSON for request body usage.
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The JSON body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+    void patch(String url, String username, String password, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Patch request using JWT Token for authentication and XML for request body usage.
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The XML body request to send.
+         * @param resCode The response code.
+         */
+    void patchXml(String url, String token, Object body, int resCode);
+
+        /**
+         * Patch request using JWT Token for authentication and XML for request body usage.
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The XML body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+    void patchXml(String url, String token, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Patch request using basic auth for authentication and XML for request body usage.
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The XML body request to send.
+         * @param resCode The response code.
+         */
+    void patchXml(String url, String username, String password, Object body, int resCode);
+
+        /**
+         * Patch request using basic auth for authentication and XML for request body usage.
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The XML body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+    void patchXml(String url, String username, String password, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Patch request using JWT Token for authentication and Multipart/form-data for request body usage.
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The Multipart/form-data body request to send.
+         * @param resCode The response code.
+         */
+    void patchMultipart(String url, String token, Object body, int resCode);
+
+        /**
+         * Patch request using JWT Token for authentication and Multipart/form-data for request body usage.
+         * @param url The URL of endpoint.
+         * @param token The JWT token.
+         * @param body The Multipart/form-data body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+    void patchMultipart(String url, String token, Object body, int resCode, String jsonSchemaPath);
+
+        /**
+         * Patch request using basic auth for authentication and Multipart/form-data for request body usage.
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The Multipart/form-data body request to send.
+         * @param resCode The response code.
+         */
+    void patchMultipart(String url, String username, String password, Object body, int resCode);
+
+        /**
+         * Post request using basic auth for authentication and Multipart/form-data for request body usage.
+         * @param url The URL of endpoint.
+         * @param username The username.
+         * @param password The password.
+         * @param body The Multipart/form-data body request to send.
+         * @param resCode The response code.
+         * @param jsonSchemaPath The JSON Schema Path. ex : "resources/jsonSchema/jsonSchemaName.json"
+         */
+    void patchMultipart(String url, String username, String password, Object body, int resCode, String jsonSchemaPath);
+
 }
